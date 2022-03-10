69d6085393fe35ccb4358aebe031abbf86772b8f
Hassan <hjammal@umich.edu>
2022-03-10 15:00:02 -0500

    Changed Integers First

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,8 +2,8 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 25;
-        int second = 35;
+        int first = 30;
+        int second = 40;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
